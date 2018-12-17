17384d6ee29439561f0e5f430102931a1aad2699
Tugdual de Kerviler <dekervit@gmail.com>
2018-12-17 16:08:41 +0100

    Bring back multilineTag prop

--- packages/editor/src/components/rich-text/index.native.js
+++ packages/editor/src/components/rich-text/index.native.js
@@ -194,8 +194,8 @@ export class RichText extends Component {
 		}
 	}
 
-	onSelectionChange( start, end, text ) {
-		this.setState( { start, end, text } );
+	onSelectionChange( start, end ) {
+		this.setState( { start, end } );
 	}
 
 	isEmpty() {
@@ -207,12 +207,14 @@ export class RichText extends Component {
 		if ( Array.isArray( value ) ) {
 			return create( {
 				html: children.toHTML( value ),
+				multilineTag: this.multilineTag,
 			} );
 		}
 
 		if ( this.props.format === 'string' ) {
 			return create( {
 				html: value,
+				multilineTag: this.multilineTag,
 			} );
 		}
 
